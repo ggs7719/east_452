3f899bba7670390636eead7bb1d595f495c07962
ggs7719 <ggs7719@gmail.com>
2017-12-18 10:42:30 +0800

    Add files via upload

---
+++ js/AutoTab.js
@@ -0,0 +1,34 @@
+﻿function TABchang(tabPanel)
+{   
+      // alert(tabPanel.id + "有"+tabPanel.tabs.length);    
+      
+        var intervalID=window.setInterval(function (){
+        var currentTabIndex=tabPanel.current_tab;
+	if(currentTabIndex+1>tabPanel.tabs.length-1)
+	{
+	  currentTabIndex=0;
+	}
+	else
+	{
+	   currentTabIndex+=1;
+	} 
+        tabPanel.current_tab=currentTabIndex;	 	
+	selectTabPanel(tabPanel, currentTabIndex,true);	   	   
+        //document.getElementById(tabPanel.tabs[currentTabIndex].tab_id).focus(); <=不能加 
+	
+  }
+   ,5000); 
+   
+   
+    this.node = document.getElementById(tabPanel.id);
+    browser.addEvent(this.node, "click", function(event) { return window.clearInterval(intervalID);}, false);
+ 
+   
+}
+
+
+
+
+
+
+ 
